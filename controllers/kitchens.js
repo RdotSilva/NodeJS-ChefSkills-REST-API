88501083081c6b4373ba1b9b017ef9e8884df732
Ryan Silva <RyandotSilva@gmail.com>
2019-10-27 11:42:44 -0400

    Add createKitchen method

--- controllers/kitchens.js
+++ controllers/kitchens.js
@@ -11,3 +11,10 @@ exports.getKitchens = (req, res, next) => {
 exports.getKitchen = (req, res, next) => {
 	res.status(200).json({ success: true, msg: `Show kitchen ${req.params.id}` });
 };
+
+// @desc      Create new kitchen
+// @route     POST /api/v1/kitchens
+// @access    Private
+exports.createKitchen = (req, res, next) => {
+	res.status(200).json({ success: true, msg: "Create new kitchen" });
+};
