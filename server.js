23f8b7df7e290c576826ac5bcc503a43a3e9c238
Ryan Silva <RyandotSilva@gmail.com>
2019-10-27 11:20:05 -0400

    Add kitchens get route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get("/api/v1/kitchens", (req, res) => {
+	res.status(200).json({ success: true, msg: "Show all kitchens" });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
