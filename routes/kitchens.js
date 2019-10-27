7389db86e8ddc67c4b5de62ea2c1f60c5aebf8db
Ryan Silva <RyandotSilva@gmail.com>
2019-10-27 11:48:01 -0400

    Set up router with controllers

--- routes/kitchens.js
+++ routes/kitchens.js
@@ -10,4 +10,15 @@ const {
 
 const router = express.Router();
 
+router
+	.route("/")
+	.get(getKitchens)
+	.post(createKitchen);
+
+router
+	.route("/:id")
+	.get(getKitchen)
+	.put(updateKitchen)
+	.delete(deleteKitchen);
+
 module.exports = router;
