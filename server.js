259512b0de27478f764af5fa2c86fb6e23e13fd9
Ryan Silva <RyandotSilva@gmail.com>
2019-10-27 11:57:39 -0400

    Replace custom logger with morgan logger

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const logger = require("./middleware/logger");
+const morgan = require("morgan");
 
 // Route files
 const kitchens = require("./routes/kitchens");
@@ -10,7 +10,10 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.use(logger);
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
 
 // Mount routers
 app.use("/api/v1/kitchens", kitchens);
