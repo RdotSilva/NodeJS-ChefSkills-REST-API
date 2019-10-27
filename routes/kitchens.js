9a6dd54e6799240d75804244a68a7b4020982642
Ryan Silva <RyandotSilva@gmail.com>
2019-10-27 11:47:42 -0400

    Import kitchens route controllers

--- routes/kitchens.js
+++ routes/kitchens.js
@@ -1,28 +1,13 @@
 const express = require("express");
+
+const {
+	getKitchens,
+	getKitchen,
+	createKitchen,
+	updateKitchen,
+	deleteKitchen
+} = require("../controllers/kitchens");
+
 const router = express.Router();
-
-router.get("/", (req, res) => {
-	res.status(200).json({ success: true, msg: "Show all kitchens" });
-});
-
-router.get("/:id", (req, res) => {
-	res.status(200).json({ success: true, msg: `Show kitchen ${req.params.id}` });
-});
-
-router.post("/", (req, res) => {
-	res.status(200).json({ success: true, msg: "Create new kitchen" });
-});
-
-router.put("/:id", (req, res) => {
-	res
-		.status(200)
-		.json({ success: true, msg: `Update kitchen ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-	res
-		.status(200)
-		.json({ success: true, msg: `Delete kitchen ${req.params.id}` });
-});
 
 module.exports = router;
