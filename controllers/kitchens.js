61e51411e03dd5d7f83e3650a6bc8c20bdba00ee
Ryan Silva <RyandotSilva@gmail.com>
2019-10-27 11:40:44 -0400

    Add getKitchen method

--- controllers/kitchens.js
+++ controllers/kitchens.js
@@ -4,3 +4,10 @@
 exports.getKitchens = (req, res, next) => {
 	res.status(200).json({ success: true, msg: "Show all kitchens" });
 };
+
+// @desc      Get single kitchen
+// @route     GET /api/v1/kitchens/:id
+// @access    Public
+exports.getKitchen = (req, res, next) => {
+	res.status(200).json({ success: true, msg: `Show kitchen ${req.params.id}` });
+};
