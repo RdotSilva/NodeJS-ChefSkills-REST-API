4dc1aeedb78707d534cac2d0ab64112fa7d6bb55
Ryan Silva <RyandotSilva@gmail.com>
2019-10-28 16:34:18 -0400

    Add name property

--- models/Kitchen.js
+++ models/Kitchen.js
@@ -1,3 +1,11 @@
 const mongoose = require("mongoose");
 
-const KitchenSchema = new mongoose.Schema({});
+const KitchenSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, "Please add a name"],
+		unique: true,
+		trim: true,
+		maxLength: [50, "Name can not be more than 50 characters"]
+	}
+});
