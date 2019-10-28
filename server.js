5d6cb49fad8b934af3783d5da15e20d624a6cbee
Ryan Silva <RyandotSilva@gmail.com>
2019-10-28 15:44:12 -0400

    Add handling for unhandled promise rejections

--- server.js
+++ server.js
@@ -24,7 +24,15 @@ app.use("/api/v1/kitchens", kitchens);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
 	PORT,
 	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+	console.log(`Error: ${err.message}`);
+
+	// Close server & exit process
+	server.close(() => process.exit(1));
+});
