a6b79c67817da2cc3730a768a6021c48064d08e3
Ryan Silva <RyandotSilva@gmail.com>
2019-10-28 16:38:05 -0400

    Add website property

--- models/Kitchen.js
+++ models/Kitchen.js
@@ -6,12 +6,19 @@ const KitchenSchema = new mongoose.Schema({
 		required: [true, "Please add a name"],
 		unique: true,
 		trim: true,
-		maxLength: [50, "Name can not be more than 50 characters"]
+		maxlength: [50, "Name can not be more than 50 characters"]
 	},
 	Slug: String,
 	description: {
 		type: String,
 		required: [true, "Please add a description"],
 		maxlength: [500, "Description can not be more than 500 characters"]
+	},
+	website: {
+		type: String,
+		match: [
+			/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+			"Please use a valid URL with HTTP or HTTPS"
+		]
 	}
 });
