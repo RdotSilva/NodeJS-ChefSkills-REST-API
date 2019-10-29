f0114e3b8aaaa0e0fdfa7bd867e6d9971a6bde3b
Ryan Silva <RyandotSilva@gmail.com>
2019-10-29 16:53:02 -0400

    Add phone property

--- models/Kitchen.js
+++ models/Kitchen.js
@@ -20,5 +20,9 @@ const KitchenSchema = new mongoose.Schema({
 			/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
 			"Please use a valid URL with HTTP or HTTPS"
 		]
+	},
+	phone: {
+		type: String,
+		maxlength: [20, "Phone number can not be longer than 20 characters"]
 	}
 });
