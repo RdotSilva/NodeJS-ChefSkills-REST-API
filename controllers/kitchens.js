d5d1a374900f8c0a0edfc368509058bc9998c6ca
Ryan Silva <RyandotSilva@gmail.com>
2019-11-01 19:49:45 -0400

    Remove try/catch & add asyncHandler

--- controllers/kitchens.js
+++ controllers/kitchens.js
@@ -1,96 +1,77 @@
 const Kitchen = require("../models/Kitchen");
+const asyncHandler = require("../middleware/async");
 const ErrorResponse = require("../utils/errorResponse");
 
 // @desc      Get all kitchens
 // @route     GET /api/v1/kitchens
 // @access    Public
-exports.getKitchens = async (req, res, next) => {
-	try {
-		const kitchens = await Kitchen.find();
+exports.getKitchens = asyncHandler(async (req, res, next) => {
+	const kitchens = await Kitchen.find();
 
-		res
-			.status(200)
-			.json({ success: true, count: kitchens.length, data: kitchens });
-	} catch (err) {
-		next(err);
-	}
-};
+	res
+		.status(200)
+		.json({ success: true, count: kitchens.length, data: kitchens });
+});
 
 // @desc      Get single kitchen
 // @route     GET /api/v1/kitchens/:id
 // @access    Public
-exports.getKitchen = async (req, res, next) => {
-	try {
-		const kitchen = await Kitchen.findById(req.params.id);
+exports.getKitchen = asyncHandler(async (req, res, next) => {
+	const kitchen = await Kitchen.findById(req.params.id);
 
-		if (!kitchen) {
-			return new ErrorResponse(
-				`Kitchen not found with id of ${req.params.id}`,
-				404
-			);
-		}
-
-		res.status(200).json({ success: true, data: kitchen });
-	} catch (err) {
-		next(err);
+	if (!kitchen) {
+		return new ErrorResponse(
+			`Kitchen not found with id of ${req.params.id}`,
+			404
+		);
 	}
-};
+
+	res.status(200).json({ success: true, data: kitchen });
+});
 
 // @desc      Create new kitchen
 // @route     POST /api/v1/kitchens
 // @access    Private
-exports.createKitchen = async (req, res, next) => {
-	try {
-		const kitchen = await Kitchen.create(req.body);
+exports.createKitchen = asyncHandler(async (req, res, next) => {
+	const kitchen = await Kitchen.create(req.body);
 
-		res.status(201).json({
-			success: true,
-			data: kitchen
-		});
-	} catch (err) {
-		next(err);
-	}
-};
+	res.status(201).json({
+		success: true,
+		data: kitchen
+	});
+});
 
 // @desc      Update kitchen
 // @route     PUT /api/v1/kitchens/:id
 // @access    Private
-exports.updateKitchen = async (req, res, next) => {
-	try {
-		const kitchen = await Kitchen.findByIdAndUpdate(req.params.id, req.body, {
-			new: true,
-			runValidators: true
-		});
+exports.updateKitchen = asyncHandler(async (req, res, next) => {
+	const kitchen = await Kitchen.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true
+	});
 
-		if (!kitchen) {
-			return new ErrorResponse(
-				`Kitchen not found with id of ${req.params.id}`,
-				404
-			);
-		}
-
-		res.status(200).json({ success: true, data: kitchen });
-	} catch (err) {
-		next(err);
+	if (!kitchen) {
+		return new ErrorResponse(
+			`Kitchen not found with id of ${req.params.id}`,
+			404
+		);
 	}
-};
+
+	res.status(200).json({ success: true, data: kitchen });
+});
 
 // @desc      Delete kitchen
 // @route     DELETE /api/v1/kitchens/:id
 // @access    Private
-exports.deleteKitchen = async (req, res, next) => {
-	try {
-		const kitchen = await Kitchen.findByIdAndDelete(req.params.id);
+exports.deleteKitchen = asyncHandler(async (req, res, next) => {
+	const kitchen = await Kitchen.findByIdAndDelete(req.params.id);
 
-		if (!kitchen) {
-			return new ErrorResponse(
-				`Kitchen not found with id of ${req.params.id}`,
-				404
-			);
-		}
-
-		res.status(200).json({ success: true, data: {} });
-	} catch (err) {
-		next(err);
+	if (!kitchen) {
+		return new ErrorResponse(
+			`Kitchen not found with id of ${req.params.id}`,
+			404
+		);
 	}
-};
+
+	res.status(200).json({ success: true, data: {} });
+});
