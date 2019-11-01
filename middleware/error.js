19e4490e475eb125db60517edc36586c502ac21e
Ryan Silva <RyandotSilva@gmail.com>
2019-11-01 19:27:19 -0400

    Add error logic for bad ObjectID

--- middleware/error.js
+++ middleware/error.js
@@ -1,9 +1,21 @@
+const ErrorResponse = require("../utils/errorResponse");
+
 const errorHandler = (err, req, res, next) => {
+	let error = { ...err };
+
+	error.message = err.message;
+
 	console.log(err.stack.red);
 
-	res.status(err.statusCode || 500).json({
+	// Mongoose bad ObjectID
+	if (err.name === "CastError") {
+		const message = `Kitchen not found with id of ${err.value}`;
+		error = new ErrorResponse(message, 404);
+	}
+
+	res.status(error.statusCode || 500).json({
 		success: false,
-		error: err.message || "Server Error"
+		error: error.message || "Server Error"
 	});
 };
 
