154347624a9d0774a7588ceecc98b7afb001a6fc
Ryan Silva <RyandotSilva@gmail.com>
2019-11-02 19:29:36 -0400

    Update logic to add address info to location

--- models/Kitchen.js
+++ models/Kitchen.js
@@ -111,7 +111,13 @@ KitchenSchema.pre("save", async function(next) {
 	const loc = await geocoder.geocode(this.address);
 	this.location = {
 		type: "Point",
-		coordinates: [loc[0].longitude, loc[0].latitude]
+		coordinates: [loc[0].longitude, loc[0].latitude],
+		formattedAddress: loc[0].formattedAddress,
+		street: loc[0].streetName,
+		city: loc[0].city,
+		state: loc[0].state,
+		zipcode: loc[0].zipcode,
+		country: loc[0].countryCode
 	};
 
 	// Do not save address in DB
