e3dab33a79c669f007c9242fc1e8fc78baacf7d7
Ryan Silva <RyandotSilva@gmail.com>
2019-11-03 18:35:01 -0500

    Add connection to mongoDB

--- seeder.js
+++ seeder.js
@@ -5,3 +5,14 @@ const dotenv = require("dotenv");
 
 // Load ENV variables
 dotenv.config({ path: "./config/config.env" });
+
+// Load models
+const Kitchen = require("./models/Kitchen");
+
+// Connect to DB.
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+	useUnifiedTopology: true
+});
