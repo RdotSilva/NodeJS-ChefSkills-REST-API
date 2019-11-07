359b81802da838d45faab8091624136118a85394
Ryan Silva <RyandotSilva@gmail.com>
2019-11-06 20:51:38 -0500

    Add sort logic

--- controllers/kitchens.js
+++ controllers/kitchens.js
@@ -13,7 +13,7 @@ exports.getKitchens = asyncHandler(async (req, res, next) => {
 	const reqQuery = { ...req.query };
 
 	// Fields to exclude
-	const removeFields = ["select"];
+	const removeFields = ["select", "sort"];
 
 	// Loop over removeFields and delete them from request copy
 	removeFields.forEach(param => delete reqQuery[param]);
@@ -31,6 +31,14 @@ exports.getKitchens = asyncHandler(async (req, res, next) => {
 	if (req.query.select) {
 		const fields = req.query.select.split(",").join(" ");
 		query = query.select(fields);
+	}
+
+	// Sort
+	if (req.query.sort) {
+		const sortBy = req.query.sort.split(",").join(" ");
+		query = query.sort(sortBy);
+	} else {
+		query = query.sort("-createdAt");
 	}
 
 	// Executing query
