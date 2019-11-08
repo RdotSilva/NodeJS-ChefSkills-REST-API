67638dfd7f824c62a2928ec04f3bfb65fc5a8cdb
Ryan Silva <RyandotSilva@gmail.com>
2019-11-08 11:48:31 -0500

    Add title & description properties

--- models/Course.js
+++ models/Course.js
@@ -1,5 +1,15 @@
 const mongoose = require("mongoose");
 
-const CourseSchema = new mongoose.Schema({});
+const CourseSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		trim: true,
+		required: [true, "Please add a course title"]
+	},
+	description: {
+		type: String,
+		required: [true, "Please add a description"]
+	}
+});
 
 module.exports = mongoose.model("Course", CourseSchema);
