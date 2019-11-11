ff0ef1c0fd7e5182f6c02cf84b0f5e6ea807b6f2
Ryan Silva <RyandotSilva@gmail.com>
2019-11-11 18:40:51 -0500

    Add fileupload middleware to server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
+const fileupload = require("express-fileupload");
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 
@@ -24,6 +25,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "development") {
 	app.use(morgan("dev"));
 }
+
+// File uploading
+app.use(fileupload());
 
 // Mount routers
 app.use("/api/v1/kitchens", kitchens);
