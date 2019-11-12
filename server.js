5b27d72e8655ae4f3b05b4dd8a3e81b037a58d65
Ryan Silva <RyandotSilva@gmail.com>
2019-11-11 19:16:21 -0500

    Add public static folder to server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
@@ -28,6 +29,9 @@ if (process.env.NODE_ENV === "development") {
 
 // File uploading
 app.use(fileupload());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 // Mount routers
 app.use("/api/v1/kitchens", kitchens);
