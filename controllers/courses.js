e698fd23a700d9cb4d5fcbf35d0b6d38af2582d2
Ryan Silva <RyandotSilva@gmail.com>
2019-11-12 18:30:56 -0500

    Update getCourses controller

--- controllers/courses.js
+++ controllers/courses.js
@@ -8,112 +8,104 @@ const ErrorResponse = require("../utils/errorResponse");
 // @route     GET /api/v1/kitchens/:kitchenId/courses
 // @access    Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-	let query;
-
-	if (req.params.kitchenId) {
-		query = Course.find({ kitchen: req.params.kitchenId });
-	} else {
-		query = Course.find().populate({
-			path: "kitchen",
-			select: "name description"
-		});
-	}
-
-	const courses = await query;
-
-	res.status(200).json({
-		success: true,
-		count: courses.length,
-		data: courses
-	});
+  if (req.params.kitchenId) {
+    const courses = await Course.find({ kitchen: req.params.kitchenId });
+    return res.status(200).json({
+      success: true,
+      count: courses.length,
+      data: courses
+    });
+  } else {
+    res.status(200).json(res.advancedResults);
+  }
 });
 
 // @desc      Get single course
 // @route     GET /api/v1/courses/:id
 // @access    Public
 exports.getCourse = asyncHandler(async (req, res, next) => {
-	const course = await Course.findById(req.params.id).populate({
-		path: "kitchen",
-		select: "name description"
-	});
+  const course = await Course.findById(req.params.id).populate({
+    path: "kitchen",
+    select: "name description"
+  });
 
-	if (!course) {
-		return next(
-			new ErrorResponse(`No course with id of ${req.params.id}`),
-			404
-		);
-	}
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with id of ${req.params.id}`),
+      404
+    );
+  }
 
-	res.status(200).json({
-		success: true,
-		data: course
-	});
+  res.status(200).json({
+    success: true,
+    data: course
+  });
 });
 
 // @desc      Add course
 // @route     POST /api/v1/kitchens/:kitchenId/courses
 // @access    Private
 exports.addCourse = asyncHandler(async (req, res, next) => {
-	req.body.kitchen = req.params.kitchenId;
+  req.body.kitchen = req.params.kitchenId;
 
-	const kitchen = await Kitchen.findById(req.params.kitchenId);
+  const kitchen = await Kitchen.findById(req.params.kitchenId);
 
-	if (!kitchen) {
-		return next(
-			new ErrorResponse(`No kitchen with id of ${req.params.kitchenId}`),
-			404
-		);
-	}
+  if (!kitchen) {
+    return next(
+      new ErrorResponse(`No kitchen with id of ${req.params.kitchenId}`),
+      404
+    );
+  }
 
-	const course = await Course.create(req.body);
+  const course = await Course.create(req.body);
 
-	res.status(200).json({
-		success: true,
-		data: course
-	});
+  res.status(200).json({
+    success: true,
+    data: course
+  });
 });
 
 // @desc      Update course
 // @route     PUT /api/v1/courses/:id
 // @access    Private
 exports.updateCourse = asyncHandler(async (req, res, next) => {
-	let course = await Course.findById(req.params.id);
+  let course = await Course.findById(req.params.id);
 
-	if (!course) {
-		return next(
-			new ErrorResponse(`No course with the id of ${req.params.id}`),
-			404
-		);
-	}
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
 
-	course = await Course.findByIdAndUpdate(req.params.id, req.body, {
-		new: true,
-		runValidators: true
-	});
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-	res.status(200).json({
-		success: true,
-		data: course
-	});
+  res.status(200).json({
+    success: true,
+    data: course
+  });
 });
 
 // @desc      Delete course
 // @route     DELETE /api/v1/courses/:id
 // @access    Private
 exports.deleteCourse = asyncHandler(async (req, res, next) => {
-	const course = await Course.findById(req.params.id);
+  const course = await Course.findById(req.params.id);
 
-	if (!course) {
-		return next(
-			new ErrorResponse(`No course with the id of ${req.params.id}`),
-			404
-		);
-	}
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
 
-	await course.remove();
+  await course.remove();
 
-	res.status(200).json({
-		success: true,
-		data: {}
-	});
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
 });
