5ce0f853d25e32a0678ff8ef0e1b74495dc806ef
Ryan Silva <RyandotSilva@gmail.com>
2019-11-13 16:26:55 -0500

    Add email key

--- models/User.js
+++ models/User.js
@@ -4,5 +4,14 @@ const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Please add a name"]
+  },
+  email: {
+    type: String,
+    required: [true, "Please add an email"],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please add a valid email"
+    ]
   }
 });
