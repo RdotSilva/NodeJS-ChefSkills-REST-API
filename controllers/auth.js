c21cdedbc579ba8a3c767dcd9daf9e1eda17433e
Ryan Silva <RyandotSilva@gmail.com>
2019-11-13 16:40:25 -0500

    Update register controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,5 +6,15 @@ const ErrorResponse = require("../utils/errorResponse");
 // @route     GET /api/v1/auth/register
 // @access    Public
 exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  // Create User
+  const user = await user.create({
+    name,
+    email,
+    password,
+    role
+  });
+
   res.status(200).json({ success: true });
 });
