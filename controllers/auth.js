6639f3710fc63c0f1a17ec67716d42ab7784bfe7
Ryan Silva <RyandotSilva@gmail.com>
2019-11-13 16:32:22 -0500

    Add register controller

---
+++ controllers/auth.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const asyncHandler = require("../middleware/async");
+const ErrorResponse = require("../utils/errorResponse");
+
+// @desc      Register user
+// @route     GET /api/v1/auth/register
+// @access    Public
+exports.register = asyncHandler(async (req, res, next) => {
+  res.status(200).json({ success: true });
+});
