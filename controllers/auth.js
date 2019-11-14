37296c138bfc0e634eed7708376fe004c95eeb66
Ryan Silva <RyandotSilva@gmail.com>
2019-11-14 18:25:36 -0500

    Fix typo

--- controllers/auth.js
+++ controllers/auth.js
@@ -35,7 +35,7 @@ exports.login = asyncHandler(async (req, res, next) => {
   }
 
   // Check for user
-  const user = await user.findOne({ email }).select("+password");
+  const user = await User.findOne({ email }).select("+password");
 
   if (!user) {
     return next(new ErrorResponse("Invalid credentials", 401));
