a4d4d802e3bc2b0204c61522a7ab97bc6033abb2
Ryan Silva <RyandotSilva@gmail.com>
2019-11-15 19:34:14 -0500

    Refactor controllers using sendTokenResponse

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,10 +17,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role
   });
 
-  // Create token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
 });
 
 // @desc      Login user
@@ -48,10 +45,7 @@ exports.login = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse("Invalid credentials", 401));
   }
 
-  // Create token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
 });
 
 // Get token from model, create cookie and send response
