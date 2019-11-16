886cc19596eb399514fb8a2f5b0ad87aa369fcd1
Ryan Silva <RyandotSilva@gmail.com>
2019-11-15 19:44:48 -0500

    Add protect middleware

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("./async");
+const ErrorResponse = require("../utils/errorResponse");
+const User = require("../models/User");
+
+// Product routes
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  // } else if (req.cookies.token) {
+  //   token = req.cookies.token;
+  // }
+
+  // Make sure token exists
+  if (!token) {
+    return next(new ErrorResponse("Not authorized to access this route", 401));
+  }
+});
