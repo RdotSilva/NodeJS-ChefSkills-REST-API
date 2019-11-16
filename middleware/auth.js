bdbb9b7de4bbdebcaa3bd7d08290ad080a30d0a6
Ryan Silva <RyandotSilva@gmail.com>
2019-11-15 19:44:56 -0500

    Add try/catch to verify token

--- middleware/auth.js
+++ middleware/auth.js
@@ -22,4 +22,15 @@ exports.protect = asyncHandler(async (req, res, next) => {
   if (!token) {
     return next(new ErrorResponse("Not authorized to access this route", 401));
   }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await User.findById(decoded.id);
+
+    next();
+  } catch (err) {
+    return next(new ErrorResponse("Not authorized to access this route", 401));
+  }
 });
