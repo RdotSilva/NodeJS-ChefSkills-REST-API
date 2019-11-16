e57c30033ffd463573ab4f79fed4df968c8e5d1c
Ryan Silva <RyandotSilva@gmail.com>
2019-11-16 17:43:03 -0500

    Update seeder to import/data user data

--- seeder.js
+++ seeder.js
@@ -9,53 +9,60 @@ dotenv.config({ path: "./config/config.env" });
 // Load models
 const Kitchen = require("./models/Kitchen");
 const Course = require("./models/Course");
+const User = require("./models/User");
 
 // Connect to DB.
 mongoose.connect(process.env.MONGO_URI, {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useFindAndModify: false,
-	useUnifiedTopology: true
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // Read JSON files
 const kitchens = JSON.parse(
-	fs.readFileSync(`${__dirname}/_data/kitchens.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/_data/kitchens.json`, "utf-8")
 );
 
 const courses = JSON.parse(
-	fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
+
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
 );
 
 // Import into DB
 const importData = async () => {
-	try {
-		await Kitchen.create(kitchens);
-		await Course.create(courses);
+  try {
+    await Kitchen.create(kitchens);
+    await Course.create(courses);
+    await User.create(users);
 
-		console.log("Data Imported...".green.inverse);
-		process.exit();
-	} catch (err) {
-		console.error(err);
-	}
+    console.log("Data Imported...".green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 // Delete data
 const deleteData = async () => {
-	try {
-		await Kitchen.deleteMany();
-		await Course.deleteMany();
+  try {
+    await Kitchen.deleteMany();
+    await Course.deleteMany();
+    await User.deleteMany();
 
-		console.log("Data destroyed...".red.inverse);
-		process.exit();
-	} catch (err) {
-		console.error(err);
-	}
+    console.log("Data destroyed...".red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 // Check arguments for flag to import or delete data
 if (process.argv[2] === "-i") {
-	importData();
+  importData();
 } else if (process.argv[2] === "-d") {
-	deleteData();
+  deleteData();
 }
