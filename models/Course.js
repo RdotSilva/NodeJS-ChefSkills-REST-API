f33bc452a20fd8069fd90e30f0733a7872257469
Ryan Silva <RyandotSilva@gmail.com>
2019-11-17 13:39:40 -0500

    Update courseschema with user key

--- models/Course.js
+++ models/Course.js
@@ -1,74 +1,79 @@
 const mongoose = require("mongoose");
 
 const CourseSchema = new mongoose.Schema({
-	title: {
-		type: String,
-		trim: true,
-		required: [true, "Please add a course title"]
-	},
-	description: {
-		type: String,
-		required: [true, "Please add a description"]
-	},
-	weeks: {
-		type: String,
-		required: [true, "Please add number of weeks"]
-	},
-	tuition: {
-		type: Number,
-		required: [true, "Please add a tuition cost"]
-	},
-	minimumSkill: {
-		type: String,
-		required: [true, "Please add a minimum skill"],
-		enum: ["beginner", "intermediate", "advanced"]
-	},
-	scholarshipsAvailable: {
-		type: Boolean,
-		default: false
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now
-	},
-	kitchen: {
-		type: mongoose.Schema.ObjectId,
-		ref: "Kitchen",
-		required: true
-	}
+  title: {
+    type: String,
+    trim: true,
+    required: [true, "Please add a course title"]
+  },
+  description: {
+    type: String,
+    required: [true, "Please add a description"]
+  },
+  weeks: {
+    type: String,
+    required: [true, "Please add number of weeks"]
+  },
+  tuition: {
+    type: Number,
+    required: [true, "Please add a tuition cost"]
+  },
+  minimumSkill: {
+    type: String,
+    required: [true, "Please add a minimum skill"],
+    enum: ["beginner", "intermediate", "advanced"]
+  },
+  scholarshipsAvailable: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  kitchen: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Kitchen",
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true
+  }
 });
 
 // Static method to get average of course tuitions
 CourseSchema.statics.getAverageCost = async function(kitchenId) {
-	const obj = await this.aggregate([
-		{
-			$match: { kitchen: kitchenId }
-		},
-		{
-			$group: {
-				_id: "$kitchen",
-				averageCost: { $avg: "$tuition" }
-			}
-		}
-	]);
+  const obj = await this.aggregate([
+    {
+      $match: { kitchen: kitchenId }
+    },
+    {
+      $group: {
+        _id: "$kitchen",
+        averageCost: { $avg: "$tuition" }
+      }
+    }
+  ]);
 
-	try {
-		await this.model("Kitchen").findByIdAndUpdate(kitchenId, {
-			averageCost: Math.ceil(obj[0].averageCost / 10) * 10
-		});
-	} catch (err) {
-		console.error(err);
-	}
+  try {
+    await this.model("Kitchen").findByIdAndUpdate(kitchenId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 // Call getAverageCost after save
 CourseSchema.post("save", function() {
-	this.constructor.getAverageCost(this.kitchen);
+  this.constructor.getAverageCost(this.kitchen);
 });
 
 // Call getAverageCost before remove
 CourseSchema.pre("remove", function() {
-	this.constructor.getAverageCost(this.kitchen);
+  this.constructor.getAverageCost(this.kitchen);
 });
 
 module.exports = mongoose.model("Course", CourseSchema);
