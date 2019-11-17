2ef2aecd788453464941b1c0433571545fe033c4
Ryan Silva <RyandotSilva@gmail.com>
2019-11-17 14:28:41 -0500

    Update forgotPassword to save user

--- controllers/auth.js
+++ controllers/auth.js
@@ -73,6 +73,8 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   // Get reset token
   const resetToken = user.getResetPasswordToken();
 
+  await user.save({ validateBeforeSave: false });
+
   res.status(200).json({
     success: true,
     data: user
