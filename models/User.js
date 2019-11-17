ae492cd0c54fbf1515e75e05b17db412c9ae7553
Ryan Silva <RyandotSilva@gmail.com>
2019-11-17 14:18:53 -0500

    Update bcrypt pre hook

--- models/User.js
+++ models/User.js
@@ -38,6 +38,10 @@ const UserSchema = new mongoose.Schema({
 
 // Encrypt pw using bcrypt
 UserSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
