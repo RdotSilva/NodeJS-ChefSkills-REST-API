26f9cdffe9aafb41bb1aa83f7f9cd4de45e81052
Ryan Silva <RyandotSilva@gmail.com>
2019-11-17 14:36:20 -0500

    Update sendEmail with message

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -10,6 +10,18 @@ const sendEmail = async options => {
       pass: process.env.SMTP_PASSWORD
     }
   });
+
+  // send mail with defined transport object
+  const message = {
+    from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message
+  };
+
+  const info = await transporter.sendMail(message);
+
+  console.log("Message sent: %s", info.messageId);
 };
 
 module.exports = sendEmail;
