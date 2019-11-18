15404c6942bdeaef5407676628b08f395cb296ff
Ryan Silva <RyandotSilva@gmail.com>
2019-11-18 17:08:45 -0500

    Change resetpassword route to put

--- routes/auth.js
+++ routes/auth.js
@@ -15,6 +15,6 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
 router.post("/forgotpassword", forgotPassword);
-router.post("/resetpassword/:resettoken", resetPassword);
+router.put("/resetpassword/:resettoken", resetPassword);
 
 module.exports = router;
