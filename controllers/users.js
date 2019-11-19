934a9a8907d591ac70e4e8c406091f0a126f4fd7
Ryan Silva <RyandotSilva@gmail.com>
2019-11-19 16:06:33 -0500

    Add getUsers controller

---
+++ controllers/users.js
@@ -0,0 +1,10 @@
+const asyncHandler = require("../middleware/async");
+const ErrorResponse = require("../utils/errorResponse");
+const User = require("../models/User");
+
+// @desc      Get all users
+// @route     GET /api/v1/users
+// @access    Private/Admin
+exports.getUsers = asyncHandler(async (req, res, next) => {
+  res.status(200).json(res.advancedResults);
+});
