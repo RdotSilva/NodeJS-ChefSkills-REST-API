b9aab4ecd9e51e05c5545cb5acf7e15a1add1f4e
Ryan Silva <RyandotSilva@gmail.com>
2019-11-19 16:08:09 -0500

    Add createUser controller

--- controllers/users.js
+++ controllers/users.js
@@ -9,13 +9,25 @@ exports.getUsers = asyncHandler(async (req, res, next) => {
   res.status(200).json(res.advancedResults);
 });
 
-// @desc      Get single users
+// @desc      Get single user
 // @route     GET /api/v1/users/:id
 // @access    Private/Admin
 exports.getUser = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.params.id);
 
   res.status(200).json({
+    success: true,
+    data: user
+  });
+});
+
+// @desc      Create user
+// @route     POST /api/v1/users/
+// @access    Private/Admin
+exports.createUser = asyncHandler(async (req, res, next) => {
+  const user = await User.create(req.body);
+
+  res.status(201).json({
     success: true,
     data: user
   });
