2263091590cc6e650c0e8ca76c725dc4f56988b7
Ryan Silva <RyandotSilva@gmail.com>
2019-11-19 16:15:04 -0500

    Add routes

--- routes/users.js
+++ routes/users.js
@@ -15,8 +15,18 @@ const router = express.Router({ mergeParams: true });
 const advancedResults = require("../middleware/advancedResults");
 const { protect, authorize } = require("../middleware/auth");
 
-router.route("/");
+router.use(protect);
+router.use(authorize("admin"));
 
-router.route("/:id");
+router
+  .route("/")
+  .get(advancedResults(User), getUsers)
+  .post(createUser);
+
+router
+  .route("/:id")
+  .get(getUser)
+  .put(updateUser)
+  .delete(deleteUser);
 
 module.exports = router;
