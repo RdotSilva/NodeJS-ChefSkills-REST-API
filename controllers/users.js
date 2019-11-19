919deb9652ee68332f8638ffd712e8e78cf8931f
Ryan Silva <RyandotSilva@gmail.com>
2019-11-19 16:07:24 -0500

    Add getUser controller

--- controllers/users.js
+++ controllers/users.js
@@ -8,3 +8,15 @@ const User = require("../models/User");
 exports.getUsers = asyncHandler(async (req, res, next) => {
   res.status(200).json(res.advancedResults);
 });
+
+// @desc      Get single users
+// @route     GET /api/v1/users/:id
+// @access    Private/Admin
+exports.getUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
