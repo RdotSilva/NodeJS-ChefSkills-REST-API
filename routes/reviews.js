e73cdbfec05ba4d67998c159125b05a3dee06595
Ryan Silva <RyandotSilva@gmail.com>
2019-11-20 18:51:45 -0500

    Add getReviews route

---
+++ routes/reviews.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const { getReviews } = require("../controllers/reviews");
+
+const Review = require("../models/Review");
+
+const router = express.Router({ mergeParams: true });
+
+const advancedResults = require("../middleware/advancedResults");
+const { protect, authorize } = require("../middleware/auth");
+
+router.route("/").get(
+  advancedResults(Review, {
+    path: "kitchen",
+    select: "name description"
+  }),
+  getReviews
+);
+
+module.exports = router;
