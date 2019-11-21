04dbc5219d91c75e7ec876e7e826127287fd5513
Ryan Silva <RyandotSilva@gmail.com>
2019-11-21 17:06:32 -0500

    Update seeder with reviews

--- seeder.js
+++ seeder.js
@@ -32,12 +32,17 @@ const users = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
 );
 
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, "utf-8")
+);
+
 // Import into DB
 const importData = async () => {
   try {
     await Kitchen.create(kitchens);
     await Course.create(courses);
     await User.create(users);
+    await Review.create(reviews);
 
     console.log("Data Imported...".green.inverse);
     process.exit();
@@ -52,6 +57,7 @@ const deleteData = async () => {
     await Kitchen.deleteMany();
     await Course.deleteMany();
     await User.deleteMany();
+    await Review.deleteMany();
 
     console.log("Data destroyed...".red.inverse);
     process.exit();
