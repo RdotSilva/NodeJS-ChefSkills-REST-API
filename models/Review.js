eacd7b696017b7245bec29e79958c0c03f9b6075
Ryan Silva <RyandotSilva@gmail.com>
2019-11-21 18:28:05 -0500

    Add getAverageRating static method

--- models/Review.js
+++ models/Review.js
@@ -36,4 +36,27 @@ const ReviewSchema = new mongoose.Schema({
 // Prevent user from submitting more than one review per kitchen
 ReviewSchema.index({ kitchen: 1, user: 1 }, { unique: true });
 
+// Static method to get average of rating and save
+ReviewSchema.statics.getAverageRating = async function(kitchenId) {
+  const obj = await this.aggregate([
+    {
+      $match: { kitchen: kitchenId }
+    },
+    {
+      $group: {
+        _id: "$kitchen",
+        averageRating: { $avg: "$rating" }
+      }
+    }
+  ]);
+
+  try {
+    await this.model("Kitchen").findByIdAndUpdate(kitchenId, {
+      averageRating: obj[0].averageRating
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 module.exports = mongoose.model("Review", ReviewSchema);
